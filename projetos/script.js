e7e77f680fe773887db2dc5c97a8949fbf7394e6
Demetrio Silva <demetrio.ts@gmail.com>
2025-07-07 15:26:46 -0300

    Adjust container width

--- projetos/script.js
+++ projetos/script.js
@@ -17,9 +17,11 @@ function toggleMode() {
     if (html.classList.contains("light")) {
         // se tiver light mode, adicionar a imagem light
         img.setAttribute("src", "./assets/avatar-light.png")
+        img.setAttribute("alt", "Foto de Mayk Brito sorrindo, usando óculos de sol e camisa preta.")
     } else {
         // se tiver sem light mode, manter a imagem normal
         img.setAttribute("src", "./assets/avatar.png")
+        img.setAttribute("alt", "Foto de Mayk Brito sorrindo, usando óculos e camisa preta.")
     }
 
     // comentaário
